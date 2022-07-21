f05e85a8bce084d9156f3f59cf0dd24125696fdb
djon125 <ross.dion9027@gmail.com>
2022-07-20 21:45:07 -0500

    added supertype serializer

--- supers/views.py
+++ supers/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
 from rest_framework import status
+
+from super_types.models import SuperType
 from .serializers import SuperSerializer
 from .models import Super
+from super_types.serializers import SuperTypeSerializer
 
 @api_view(['GET', 'POST'])
 def supers_list(request):
     
     if request.method == 'GET':
         
-        supe_type = request.query_params.get('type')
-        print(supe_type)
-        
+        supe_type = request.query_params.get('type')        
         supers = Super.objects.all()
         if supe_type:
             supers = supers.filter(super_type__type=supe_type)
@@ -40,3 +41,17 @@ def super_detail(request, pk):
         super.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
+# @api_view(['GET'])
+# def get_custom_dict(request):
+#    supes = Super.ojbects.all()
+#    supetype = SuperType.objects.all()
+   
+#    supes_serializer = SuperSerializer(supes, many=True)
+#    supetype_serializer = SuperTypeSerializer(supetype, many=True)
+   
+#    custom_dict_response ={
+#        'heroes': supes_serializer.data,
+#        'villains': supetype_serializer.data
+#    }
+
+#    return Response(custom_dict_response)
